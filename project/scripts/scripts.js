126458ae3a150380147aa6ccf9dde835e1752c27
Anastasiia Kulachok <kulachokn@gmail.com>
2020-01-01 18:52:51 +0200

    added asset content upload

--- project/scripts/scripts.js
+++ project/scripts/scripts.js
@@ -1,5 +1,171 @@
 'use strict';
 
+class AssetRepository {
+
+  constructor() {
+    this.db = new PouchDB('assets');
+    this.CONTENT_TYPES = {
+      BOOK: 'BOOK',
+      VIDEO: 'VIDEO',
+      AUDIO: 'AUDIO'
+    }
+  }
+
+  async create(asset) {
+    console.log(asset);
+
+    const draftAsset = {
+      title: asset.title,
+      level: asset.level,
+      language: asset.language,
+      createdAt: Date.now(),
+      tags: asset.tags,
+      description: asset.description,
+      rating: [],
+      contentType: asset.contentType,
+      createdBy: asset.user,
+      _attachments: {
+        [asset.content.name]: {
+          content_type: asset.content.type,
+          data: asset.content
+        }
+      }
+    };
+
+    if (asset.thumbnail.isDefault) {
+      draftAsset.thumbnail = asset.thumbnail.url;
+    } else {
+      draftAsset._attachments.thumbnail = {
+        data: asset.thumbnail.url,
+        content_type: asset.thumbnail.contentType
+      }
+    }
+
+    const response = await this.db.post(draftAsset);
+    console.log(response);
+    return response;
+  }
+
+  async getOne(id) {
+    const response = await this.db.getAttachment(id, 'План_тренувань_CF_Banda.pdf');
+    console.log(response);
+    return response;
+  }
+}
+
+class AssetController {
+
+  constructor() {
+    this.assetRepository = new AssetRepository();
+    this.addAsset = this.addAsset.bind(this);
+  }
+
+  async addAsset(event) {
+    event.preventDefault();
+    const titleField = document.querySelector('.add-content-form input[name=title]');
+    const descriptionField = document.querySelector('.add-content-form textarea[name=description]');
+    const tagsField = document.querySelector('.add-content-form input[name=tags]');
+    const contentField = document.querySelector('.add-content-form input[name=file]');
+    const thumbnailField = document.querySelector('.add-content-form input[name=thumbnail]');
+    const levelField = [].filter.call(document.querySelectorAll('.add-content-form input[name=level]'), function (node) { return node.checked })[0];
+    const languageField = [].filter.call(document.querySelectorAll('.add-content-form input[name=language]'), function (node) { return node.checked })[0];
+
+    const title = titleField.value;
+    const description = descriptionField.value;
+    const tags = tagsField.value;
+    const content = contentField.files[0];
+    let thumbnail = thumbnailField.files[0];
+    const level = levelField.value;
+    const language = languageField.value;
+    const user = JSON.parse(localStorage.getItem('user'));
+    const contentType = this._identifyContentType(content);
+
+    if (title.length < 3) {
+      console.error('Title should be at least 3 chars length!');
+      return;
+    }
+
+    if (!content) {
+      console.error('Content is required!');
+      return;
+    }
+
+    if (!thumbnail) {
+      thumbnail = {
+        url: this._chooseDefaultThumbnail(contentType),
+        isDefault: true,
+      };
+    } else {
+      thumbnail = {
+        url: this._readThumbnail(thumbnail),
+        contentType: thumbnail.type,
+        isDefault: false
+      };
+    }
+
+    await this.assetRepository.create({
+      title,
+      description,
+      tags,
+      content,
+      thumbnail,
+      level,
+      language,
+      user,
+      contentType
+    });
+
+  }
+
+  async showContent() {
+    const response = await this.assetRepository.getOne('d3184b00-64bf-4bdf-8dc8-0253fbbc0ef3');
+    const url = window.URL.createObjectURL(response);
+    const iframe = document.createElement('iframe');
+    iframe.width = 600;
+    iframe.height = 800;
+    iframe.src = url;
+    document.body.appendChild(iframe);
+  }
+
+  _identifyContentType(content) {
+    if (content.type.includes('application')) {
+      return this.assetRepository.CONTENT_TYPES.BOOK;
+    }
+    if (content.type.includes('video')) {
+      return this.assetRepository.CONTENT_TYPES.VIDEO;
+    }
+    if (content.type.includes('audio')) {
+      return this.assetRepository.CONTENT_TYPES.AUDIO;
+    }
+
+    throw new Error('Unsupported content type');
+  }
+
+  _chooseDefaultThumbnail(contentType) {
+    switch(contentType) {
+      case this.assetRepository.CONTENT_TYPES.BOOK:
+        return '/img/default-book-thumbnail.jpg';
+      case this.assetRepository.CONTENT_TYPES.AUDIO:
+        return '/img/default-audio-thumbnail.jpg';
+      case this.assetRepository.CONTENT_TYPES.VIDEO:
+        return '/img/default-video-thumbnail.jpg';
+      default:
+        throw new Error(`Doesn't have thumbnail for provided content type "${contentType}"`);
+    }
+  }
+
+  _readThumbnail(file) {
+    return new Promise((ok, fail) => {
+      const reader = new FileReader();
+      reader.onloadend = function() {
+        ok(reader.result);
+      };
+      reader.onerror = fail;
+      reader.readAsDataURL(file);
+    });
+  }
+}
+
 class UserRepository {
   constructor() {
     this.db = new PouchDB('users');
@@ -101,7 +267,7 @@ class UserController {
     localStorage.setItem('user', JSON.stringify({
       name: response.name,
       email: response.email,
-      id: response.id
+      id: response._id
     }));
     document.location = 'index.html';
   }
@@ -157,7 +323,7 @@ class UserController {
     localStorage.setItem('user', JSON.stringify({
       name: response.name,
       email: response.email,
-      id: response.id
+      id: response._id
     }));
 
     emailField.value = '';
@@ -168,6 +334,7 @@ class UserController {
 }
 
 const userController = new UserController();
+const assetController = new AssetController();
 
 userController.readCurrentUser();
 
@@ -184,4 +351,9 @@ logoutButton.onclick = userController.logOut;
 const logInForm = document.querySelector('.sign-in .registration-form');
 if (logInForm) {
   logInForm.onsubmit = userController.logIn;
+}
+
+const addAssetForm = document.querySelector('.add-content-form');
+if (addAssetForm) {
+  addAssetForm.onsubmit = assetController.addAsset;
 }
