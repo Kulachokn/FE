5e69dedd8c7c2824e1a938077e2b1c66adfb8d3f
Anastasiia Kulachok <kulachokn@gmail.com>
2020-01-10 13:31:47 +0200

    added inputs on add-content page

--- project/scripts/input-test.js
+++ project/scripts/input-test.js
@@ -1,24 +1,22 @@
-var inputs = document.querySelectorAll('.inputfile');
+let inputs = document.querySelectorAll('.input-type-file');
 Array.prototype.forEach.call(inputs, function(input){
-  var label	 = input.nextElementSibling,
+  let label	 = input.nextElementSibling,
     labelVal = label.innerHTML;
   input.addEventListener('change', function(e){
-    var fileName = '';
-    if( this.files && this.files.length > 1 )
-      fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
-    else
-      fileName = e.target.value.split( '\\' ).pop();
+    let fileName = '';
+    // if( this.files && this.files.length > 1 )
+    //   fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+    // else
+    //   fileName = e.target.value.split( '\\' ).pop();
+
+    fileName = e.target.files[0].name;
     if( fileName )
       label.querySelector( 'span' ).innerHTML = fileName;
     else
       label.innerHTML = labelVal;
   });
+  input.addEventListener('focus', function(){ input.classList.add( 'has-focus' ); });
+  input.addEventListener('blur', function(){ input.classList.remove( 'has-focus' ); });
+
 });
 
-// input.addEventListener('focus', function(){ input.classList.add( 'has-focus' ); });
-// input.addEventListener('blur', function(){ input.classList.remove( 'has-focus' ); });
-// .inputfile:focus + label,
-// .inputfile.has-focus + label {
-//   outline: 1px dotted #000;
-//   outline: -webkit-focus-ring-color auto 5px;
-// }
